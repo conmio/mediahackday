e40da60c59510f90eaffd89747b366f06a5d2764
Matti Anthony Lahtinen <matti.lahtinen@conmio.com>
2013-10-05 18:16:48 +0200

    Rescue complete

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,13 +3,11 @@ import com.conmio.mediahackday.headline.InMemHeadlineRespository
 
 // Place your Spring DSL code here
 beans = {
-<<<<<<< HEAD
     authenticationService{bean->
         bean.autowire = 'byName'
         bean.parent = ref("mockupAuthenticationService")
 
     }
-=======
     articleRepository(InMemArticleRepository) {bean ->
             bean.autowire = 'byName'
     }
@@ -18,5 +16,4 @@ beans = {
             bean.autowire = 'byName'
     }
 
->>>>>>> 5a80dc57f55a8e24b8b489d81b4f278214fdefe4
 }
--- grails-app/controllers/mediahackday/MainController.groovy
+++ grails-app/controllers/mediahackday/MainController.groovy
@@ -4,19 +4,14 @@ import com.conmio.mediahackday.headline.Headline
 
 class MainController {
     def httpService
-
     static defaultAction = "index"
+
+
     def index() {
-<<<<<<< HEAD
-        log.info "User has arrived to main Controller."
-        render view: "/pages/index"
-       // render " This is a page."
-=======
 
         List<Headline> headlines =  httpService.getHeadlinesData()
-       // render view: "/index"
+        // render view: "/index"
         println("headlines: " + headlines.size())
         render(view: "/index", model: [headlines: headlines])
->>>>>>> 5a80dc57f55a8e24b8b489d81b4f278214fdefe4
     }
 }
