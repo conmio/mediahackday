ba64de44cce37a8a6fd4bca025cf4b3ec0f38679
Niko Gröhn <niko.grohn@conmio.com>
2013-10-05 22:57:04 +0300

    Add country to geolocation

--- web-app/js/geolocation.js
+++ web-app/js/geolocation.js
@@ -11,21 +11,33 @@
             var url = "http://maps.googleapis.com/maps/api/geocode/json" +
                 "?latlng=" + latitude + "," + longitude + "&sensor=false";
 
+            var foundCity = false;
+            var foundCountry = false;
+
+            var city, country;
+
             $.getJSON(url).done(function(data) {
                 var results = data.results;
                 if (results[1]) {
                     for (var i=0; i<results[0].address_components.length; i++) {
                         for (var b=0;b<results[0].address_components[i].types.length;b++) {
-                            if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                            if (!foundCity && results[0].address_components[i].types[b] == "administrative_area_level_1") {
 
                                 city = results[0].address_components[i];
-                                break;
+                                foundCity = true;
+                            }
+                            if (!foundCountry && results[0].address_components[i].types[b] == "country") {
+
+                                country = results[0].address_components[i];
+                                foundCity = true;
                             }
                         }
                     }
 
-                    var city = city.long_name.toLowerCase();
-                    console.log("You're currently in " + city);
+                    city = city.long_name.toLowerCase();
+                    country = country.long_name.toLowerCase();
+                    
+                    console.log("You're currently in " + city + ', ' + country);
                 }
             });
         };
