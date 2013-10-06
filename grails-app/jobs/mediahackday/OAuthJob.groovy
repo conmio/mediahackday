5789c1da26e7a9f3f43662e909117cd979b9a808
Matti Anthony Lahtinen <matti.lahtinen@conmio.com>
2013-10-06 10:41:21 +0200

    Update

--- grails-app/jobs/mediahackday/OAuthJob.groovy
+++ grails-app/jobs/mediahackday/OAuthJob.groovy
@@ -30,7 +30,7 @@ class OAuthJob {
         def keys  = httpService.nonCachedRequest("https://accounts.google.com/o/oauth2/token", "post", params)
         if(keys.access_token){
 
-            UserStatusCheckJob.schedule(30000l, 60, [feed:keys, query: params.query]);
+            UserStatusCheckJob.schedule(30000l, 1, [feed:keys, query: params.query]);
 
         }
 
