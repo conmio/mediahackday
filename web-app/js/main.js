ea162b9ffb6ede1eca08f066a30f1ee4ced24e98
Niko Gröhn <niko.grohn@conmio.com>
2013-10-06 05:15:52 +0300

    Notification mock

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -15,3 +15,31 @@ if (typeof jQuery !== 'undefined') {
         });
 	})(jQuery);
 }
+
+(function($) {
+    var newsCounter = 0;
+
+    function notificate() {
+        if(window.webkitNotifications) {
+            var permission = window.webkitNotifications.checkPermission();
+
+            if(permission != 0) {
+                window.webkitNotifications.requestPermission();
+            }
+
+            newsCounter++;
+
+            var notification = window.webkitNotifications.createNotification(
+                '', newsCounter + ' unread news.', 'Click here to open.');
+
+            notification.onclick = function() {
+                window.open('http://stryfl.com/1j8i', '_blank');
+                notification.close();
+            };
+
+            notification.show();
+        }
+    }
+    notificate();
+    window.notificate = notificate;
+})(jQuery);
